7c1ca48938acf8b090b1e3b2dce8ea2b92c4c111
Thiago Pontes <github@thiago.me>
2016-04-07 18:04:22 -0400

    setup projectile to work as ctrlp

--- configs/projectile-config.el
+++ configs/projectile-config.el
@@ -1,10 +1,11 @@
- (require 'grizzl)
- (projectile-global-mode)
- (setq projectile-enable-caching t)
- (setq projectile-completion-system 'grizzl)
- ;; Press Ctrl-p for fuzzy find in project
- (global-set-key (kbd "M-O C-M-@") 'projectile-find-file)
- ;; Press Command-b for fuzzy switch buffer
- (global-set-key (kbd "s-b") 'projectile-switch-to-buffer)
+(require 'evil)
+(require 'grizzl)
+(projectile-global-mode)
+(setq projectile-enable-caching t)
+(setq projectile-completion-system 'grizzl)
+;; Press Command-b for fuzzy switch buffer
+(global-set-key (kbd "s-b") 'projectile-switch-to-buffer)
+;; Press Ctrl-p for fuzzy find in project
+(define-key evil-normal-state-map (kbd "C-p") 'projectile-find-file)
 
 (provide 'projectile-config)
